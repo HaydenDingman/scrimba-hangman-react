d99cc5ed4e7501abb31957cb195b9431fbab348b
Hayden Dingman <haydendingman@gmail.com>
2025-07-30 11:44:21 -0700

    Add initial styling and header

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main>
+      <h1 className="title">Assembly: Endgame</h1>
+      <p className="instructions">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+    </main>
   )
 }
 
