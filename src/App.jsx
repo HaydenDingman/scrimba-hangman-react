5ba066753eed63b91b026278746a23b66dcbb3d1
Hayden Dingman <haydendingman@gmail.com>
2025-07-30 17:20:51 -0700

    Add and style list of languages from language.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
+import { languages } from './languages'
 import './App.css'
 
 function App() {
+  
+  const languageElems = languages.map(language => {
+      return (
+        <div className="language" style={{backgroundColor: language.backgroundColor, color: language.color}}>{language.name}</div>
+      )
+  })
 
   return (
     <main>
@@ -12,6 +19,9 @@ function App() {
       <section className="status">
         <h2>You win!</h2>
         <h3>Well done! 🎉</h3>
+      </section>
+      <section className="language-container">
+        {languageElems}
       </section>
     </main>
   )
